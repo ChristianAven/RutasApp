f8289894683b341bbed80fdcc6a99e33483b0248
Christian Avendaño <chrisavendano0508@gmail.com>
2021-07-22 13:55:05 -0500

    check and request GPS permission Android and IOS

--- src/screen/PermissionScreen.tsx
+++ src/screen/PermissionScreen.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
+import { check, PERMISSIONS, PermissionStatus, request } from 'react-native-permissions';
 
 const PermissionScreen = () => {
+
+    const checkLocationPermission = async() => {
+
+        let permissionStatus: PermissionStatus;
+
+        if (Platform.OS === 'ios') {
+            
+            // permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+            permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+
+        } else {
+            // permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+            permissionStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+
+            
+        }
+    }
+
+
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Permission Screen</Text>
+
+            <Button
+               title='Permission'
+               onPress={ checkLocationPermission } 
+            />
         </View>
     )
 }
